3a488beeabc6ae1662c9238dfec661cfae0e965a
Javier Aguilar <coyote176@msn.com>
2020-08-29 00:53:58 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,6 +2,12 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer 
-
+  include Dancer 
+  
+  attr_accessor :name 
+  
+  def initializer(name)
+    @name= name 
+  end
 
 end
